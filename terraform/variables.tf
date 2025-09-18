a468c6a8125dde7388155009dc3b213d90c1b2c8
Zdravko Kostov <zdravko.kostov@cloudoffice.bg>
2025-09-18 15:13:19 +0300

    lets add those files

---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "project_id" {
+  type        = string
+  description = "The ID of the GCP project to create resources in."
+}
+
+variable "region" {
+  type        = string
+  description = "The GCP region to create resources in."
+  default     = "us-central1"
+}
+
+variable "zone" {
+  type        = string
+  description = "The GCP zone to create resources in."
+  default     = "us-central1-c"
+}
+
+variable "machine_type" {
+  type        = string
+  description = "The machine type for the VMs."
+  default     = "e2-medium"
+}
+
+variable "image" {
+  type        = string
+  description = "The boot disk image for the VMs."
+  default     = "debian-cloud/debian-11"
+}
+
+variable "service_account_id" {
+  type        = string
+  description = "The ID for the custom service account."
+  default     = "task-sa"
+}
