81e9874e95f0dc4e1e44e67024e94ed2d6086b7f
Zdravko Kostov <zdravko.kostov@cloudoffice.bg>
2025-09-19 07:02:51 +0000

    first commit of this project

---
+++ src/main/java/com/onboarding/project/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.onboarding.project.controller;
+
+import com.onboarding.project.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    private final HelloWorldService helloWorldService;
+
+    @Autowired
+    public HelloWorldController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return helloWorldService.getHelloWorld();
+    }
+}
---
+++ src/main/java/com/onboarding/project/service/HelloWorldService.java
@@ -0,0 +1,11 @@
+package com.onboarding.project.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+    public String getHelloWorld() {
+        return "Hello World";
+    }
+}
